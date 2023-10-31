430f40ef16ba1ab77cd84b4f7409a40dfc846861
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2023-10-31 16:10:40 +0530

    "CLIENTS SAYS ABOUT US?" create

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 const bannerSlide = document.querySelectorAll(".img-1");
 const toggleBtn = document.querySelectorAll(".toggle-btn");
 
+window.addEventListener("scroll", function () {
+  var header = document.querySelector(".nav-bar");
+  var logo = document.querySelector(".navbar-logo");
+  var nav = document.querySelector(".navbar-list");
+
+  var isScrolled = window.scrollY > 0;
+
+  header.classList.toggle("sticky", isScrolled);
+  logo.classList.toggle("sticky", isScrolled);
+  navItems.forEach(function(navItem) {
+    navItem.classList.toggle("sticky", isScrolled);
+  });
+});
+
 function sliderfun(n) {
     let i;
     for (i = 0; i < bannerSlide.length; i++) {
